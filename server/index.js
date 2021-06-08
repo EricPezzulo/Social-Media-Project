d1c9b87b5ef0ffdf22ac87ccb485dfb0d522fc4b
Eric Pezzulo <eric.pezzulo@gmail.com>
2021-06-08 18:58:38 -0400

    fixed mongoose error

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 mongoose.connect(
   process.env.MONGO_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
   () => {
     console.log("Connected to MonogDB through Mongoose!");
   }
